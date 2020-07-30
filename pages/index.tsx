258358749845656683f4124dae8c1f413814399b
Ginger Ben <ben.wilson@aerian.com>
2020-07-30 20:53:55 +0100

    use json form data

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,18 @@ import styles from "../styles/Home.module.css";
 import { getGithubPreviewProps, parseJson } from "next-tinacms-github";
 import { GetStaticProps } from "next";
 
+import { usePlugin } from "tinacms";
+import { useGithubJsonForm } from "react-tinacms-github";
+
 export default function Home({ file }) {
-  const data = file.data;
+  const formOptions = {
+    label: "Home Page",
+    fields: [{ name: "title", component: "text" }],
+  };
+
+  // Registers a JSON Tina Form
+  const [data, form] = useGithubJsonForm(file, formOptions);
+  usePlugin(form);
 
   return (
     <div className={styles.container}>
@@ -66,30 +76,3 @@ export default function Home({ file }) {
     </div>
   );
 }
-
-/**
- * Fetch data with getStaticProps based on 'preview' mode
- */
-export const getStaticProps: GetStaticProps = async function ({
-  preview,
-  previewData,
-}) {
-  if (preview) {
-    return getGithubPreviewProps({
-      ...previewData,
-      fileRelativePath: "content/home.json",
-      parse: parseJson,
-    });
-  }
-  return {
-    props: {
-      sourceProvider: null,
-      error: null,
-      preview: false,
-      file: {
-        fileRelativePath: "content/home.json",
-        data: (await import("../content/home.json")).default,
-      },
-    },
-  };
-};
