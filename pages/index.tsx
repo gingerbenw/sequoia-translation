a338801fa9b408cefd3f58edabfeb5f9489a7665
Ginger Ben <ben@topwoodstudios.co.uk>
2020-08-28 10:35:22 +0100

    update styles and create eslint config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,116 +1,160 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import { MarkdownFieldPlugin } from "react-tinacms-editor";
-import { usePlugins } from "tinacms";
-import { getGithubPreviewProps, parseJson } from "next-tinacms-github";
-import { GetStaticProps } from "next";
+import React from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { MarkdownFieldPlugin } from 'react-tinacms-editor';
+import { usePlugins } from 'tinacms';
+import { getGithubPreviewProps, parseJson } from 'next-tinacms-github';
+import { GetStaticProps } from 'next';
 
 import {
-  InlineText,
-  InlineForm,
-  InlineTextarea,
-} from "react-tinacms-inline";
-import { usePlugin } from "tinacms";
-import { useGithubJsonForm } from "react-tinacms-github";
+	InlineText,
+	InlineForm,
+	InlineTextarea,
+} from 'react-tinacms-inline';
+import { usePlugin } from 'tinacms';
+import { useGithubJsonForm } from 'react-tinacms-github';
+import ParallaxImage from '../components/ParallaxImage';
 
 export default function Home({ file }) {
-  const formOptions = {
-    label: "Home Page",
-    fields: [
-      { name: "title", component: "text" },
-      { name: "hero", component: "textarea" },
-    ],
-  };
+	const formOptions = {
+		label: 'Home Page',
+		fields: [
+			{ name: 'title', component: 'text' },
+			{ name: 'hero', component: 'textarea' },
+		],
+	};
 
-  usePlugins([MarkdownFieldPlugin]);
+	usePlugins([MarkdownFieldPlugin]);
 
-  // Registers a JSON Tina Form
-  const [data, form] = useGithubJsonForm(file, formOptions);
-  usePlugin(form);
+	// Registers a JSON Tina Form
+	const [data, form] = useGithubJsonForm(file, formOptions);
+	usePlugin(form);
 
-  return (
-    <div >
-      <Head>
-        <title>{data.title}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div >
+			<Head>
+				<title>{data.title}</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main className={styles.main}>
-        <InlineForm form={form}>
-          {/* TODO: Convert to header component */}
-          <header>
-            <h1 className={styles.title}>
-              <InlineText name="title" />
-            </h1>
-            <div />
-          </header>
+			<InlineForm form={form}>
+				<main className={styles.main}>
+          
+					{/* TODO: Convert to header component */}
+					<header className={styles.header}>
+						<div className={styles.header_inner}>
+							<h1 className={styles.title}>
+								<InlineText name="title" />
+							</h1>
+						</div>
+					</header>
 
-          {/* TODO: Convert to hero component */}
-          <div className={styles.hero}>
-            <InlineTextarea name="hero" />
-          </div>
+					{/* TODO: Convert to hero component */}
+					<div className={styles.hero}>
+						<div className={styles.hero_text}> 
+							<InlineTextarea name="hero" />
+						</div>
+					</div>
 
-          <div className={styles.about}>
-            <h2>
-              <InlineText name="aboutTitle" />
-            </h2>
-            <InlineTextarea name="aboutText" />
-          </div>
+					<section className={styles.about}>
 
-          {/* TODO: Convert to BLOCKS */}
-          <div className={styles.container}>
+						<div className={styles.about_intro}>
+							<h2>
+								<InlineText name="servicesTitle" />
+							</h2>
+							<InlineTextarea name="servicesText" />
+						</div>
 
-          <div className={styles.row}>
-            <div className={styles.col}>
-              <h3>
-                <InlineText name="blockOneTitle" />
-              </h3>
-              <InlineTextarea name="blockOneText" />
-            </div>
-            <div className={styles.col}>
-              <h3>
-                <InlineText name="blockTwoTitle" />
-              </h3>
-              <InlineTextarea name="blockTwoText" />
-            </div>
-            <div className={styles.col}>
-              <h3>
-                <InlineText name="blockThreeTitle" />
-              </h3>
-              <InlineTextarea name="blockThreeText" />
-            </div>
-          </div>
-          </div>
+						{/* TODO: Convert to BLOCKS */}
+						<div className={styles.container}>
+							<div className={styles.row}>
+								<div className={styles.col}>
+									<h3>
+										<InlineText name="blockOneTitle" />
+									</h3>
+									<InlineTextarea name="blockOneText" />
+								</div>
+								<div className={styles.col}>
+									<h3>
+										<InlineText name="blockTwoTitle" />
+									</h3>
+									<InlineTextarea name="blockTwoText" />
+								</div>
+								<div className={styles.col}>
+									<h3>
+										<InlineText name="blockThreeTitle" />
+									</h3>
+									<InlineTextarea name="blockThreeText" />
+								</div>
+							</div>
+						</div>
+					</section>
 
-        </InlineForm>
-      </main>
-    </div>
-  );
+          <div className={styles.image_break} />
+
+					<section className={styles.about}>
+
+						<div className={styles.about_intro}>
+							<h2>
+								<InlineText name="aboutTitle" />
+							</h2>
+							<InlineTextarea name="aboutText" />
+						</div>
+
+						{/* TODO: Convert to BLOCKS */}
+						<div className={styles.container}>
+							<div className={styles.row}>
+								<div className={styles.col}>
+									<h3>
+										<InlineText name="blockFourTitle" />
+									</h3>
+									<InlineTextarea name="blockFourText" />
+								</div>
+								<div className={styles.col}>
+									<h3>
+										<InlineText name="blockFiveTitle" />
+									</h3>
+									<InlineTextarea name="blockFiveText" />
+								</div>
+								<div className={styles.col}>
+									<h3>
+										<InlineText name="blockSixTitle" />
+									</h3>
+									<InlineTextarea name="blockSixText" />
+								</div>
+							</div>
+						</div>
+					</section>
+
+				</main>
+			</InlineForm>
+		</div>
+	);
 }
 
 /**
  * Fetch data with getStaticProps based on 'preview' mode
  */
 export const getStaticProps: GetStaticProps = async function ({
-  preview,
-  previewData,
+	preview,
+	previewData,
 }) {
-  if (preview) {
-    return getGithubPreviewProps({
-      ...previewData,
-      fileRelativePath: "content/home.json",
-      parse: parseJson,
-    });
-  }
-  return {
-    props: {
-      sourceProvider: null,
-      error: null,
-      preview: false,
-      file: {
-        fileRelativePath: "content/home.json",
-        data: (await import("../content/home.json")).default,
-      },
-    },
-  };
+	if (preview) {
+		return getGithubPreviewProps({
+			...previewData,
+			fileRelativePath: 'content/home.json',
+			parse: parseJson,
+		});
+	}
+	return {
+		props: {
+			sourceProvider: null,
+			error: null,
+			preview: false,
+			file: {
+				fileRelativePath: 'content/home.json',
+				data: (await import('../content/home.json')).default,
+			},
+		},
+	};
 };
