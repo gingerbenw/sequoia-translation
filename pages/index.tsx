4f3d33c7cb1f16e9b96f2815f8cbde45a8b15e2a
asharonbaltazar <asharonbaltazar@outlook.com>
2020-09-07 11:30:17 -0500

    Added scroll to section links. Links styled

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,178 +1,202 @@
-import React from 'react';
-import Head from 'next/head';
-import styles from '../styles/Home.module.scss';
-import { MarkdownFieldPlugin } from 'react-tinacms-editor';
-import { usePlugins } from 'tinacms';
-import { getGithubPreviewProps, parseJson } from 'next-tinacms-github';
-import { GetStaticProps } from 'next';
+import React from "react";
+import ScrollIntoView from "react-scroll-into-view";
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+import { MarkdownFieldPlugin } from "react-tinacms-editor";
+import { usePlugins } from "tinacms";
+import { getGithubPreviewProps, parseJson } from "next-tinacms-github";
+import { GetStaticProps } from "next";
 
-import {
-	InlineText,
-	InlineForm,
-	InlineTextarea,
-} from 'react-tinacms-inline';
-import { usePlugin } from 'tinacms';
-import { useGithubJsonForm } from 'react-tinacms-github';
-import Hero from '../components/Hero';
-import ContactForm from '../components/ContactForm';
+import { InlineText, InlineForm, InlineTextarea } from "react-tinacms-inline";
+import { usePlugin } from "tinacms";
+import { useGithubJsonForm } from "react-tinacms-github";
+import Hero from "../components/Hero";
+import ContactForm from "../components/ContactForm";
 
 export default function Home({ file }) {
-	const formOptions = {
-		label: 'Home Page',
-		fields: [
-			{ name: 'title', component: 'text' },
-			{ name: 'hero', component: 'textarea' },
-		],
-	};
+  const formOptions = {
+    label: "Home Page",
+    fields: [
+      { name: "title", component: "text" },
+      { name: "hero", component: "textarea" },
+    ],
+  };
 
-	usePlugins([MarkdownFieldPlugin]);
+  usePlugins([MarkdownFieldPlugin]);
 
-	// Registers a JSON Tina Form
-	const [data, form] = useGithubJsonForm(file, formOptions);
-	usePlugin(form);
+  // Registers a JSON Tina Form
+  const [data, form] = useGithubJsonForm(file, formOptions);
+  usePlugin(form);
 
-	return (
-		<div >
-			<Head>
-				<title>{data.title}</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+  // Click to scroll to section
+  const handleScroll = (id) => {};
 
-			<InlineForm form={form}>
-				<main className={styles.main}>
-          
-					{/* TODO: Convert to header component */}
-					<header className={styles.header}>
-						<div className={styles.header_inner}>
-							<h1 className={styles.title}>
-								<InlineText name="title" />
-							</h1>
-						</div>
-					</header>
+  return (
+    <div>
+      <Head>
+        <title>{data.title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-					{/* TODO: Convert to hero component */}
-					<Hero />
-	
-					<section className={styles.about}>
+      <InlineForm form={form}>
+        <main className={styles.main}>
+          {/* TODO: Convert to header component */}
+          <header className={styles.header}>
+            <div className={styles.header_inner}>
+              <h1 className={styles.title}>
+                <InlineText name="title" />
+              </h1>
 
-						<div className={styles.about_intro}>
-							<h2>
-								<InlineText name="servicesTitle" />
-							</h2>
-							<InlineTextarea name="servicesText" />
-						</div>
+              {/* The link buttons */}
+              <ul>
+                <ScrollIntoView selector="#services">
+                  <li
+                    className="header_link"
+                    onClick={(e) => handleScroll(e.target)}
+                  >
+                    {data.link_services}
+                  </li>
+                </ScrollIntoView>
 
-						{/* TODO: Convert to BLOCKS */}
-						<div className={styles.container}>
-							<div className={styles.row}>
-								<div className={styles.col}>
-									<h3>
-										<InlineText name="blockOneTitle" />
-									</h3>
-									<InlineTextarea name="blockOneText" />
-								</div>
-								<div className={styles.col}>
-									<h3>
-										<InlineText name="blockTwoTitle" />
-									</h3>
-									<InlineTextarea name="blockTwoText" />
-								</div>
-								<div className={styles.col}>
-									<h3>
-										<InlineText name="blockThreeTitle" />
-									</h3>
-									<InlineTextarea name="blockThreeText" />
-								</div>
-							</div>
-						</div>
-					</section>
+                <ScrollIntoView selector="#about">
+                  <li
+                    className="header_link"
+                    onClick={(e) => handleScroll(e.target)}
+                  >
+                    {data.link_about}
+                  </li>
+                </ScrollIntoView>
+                <ScrollIntoView selector="#contact">
+                  <li
+                    className="header_link"
+                    onClick={(e) => handleScroll(e.target)}
+                  >
+                    {data.link_contact}
+                  </li>
+                </ScrollIntoView>
+              </ul>
+            </div>
+          </header>
 
-					<div className={styles.image_break} />
+          {/* TODO: Convert to hero component */}
+          <Hero />
 
-					<section className={styles.about}>
+          <section className={styles.about} id="services">
+            <div className={styles.about_intro}>
+              <h2>
+                <InlineText name="servicesTitle" />
+              </h2>
+              <InlineTextarea name="servicesText" />
+            </div>
 
-						<div className={styles.about_intro}>
-							<h2>
-								<InlineText name="aboutTitle" />
-							</h2>
-							<InlineTextarea name="aboutText" />
-						</div>
+            {/* TODO: Convert to BLOCKS */}
+            <div className={styles.container}>
+              <div className={styles.row}>
+                <div className={styles.col}>
+                  <h3>
+                    <InlineText name="blockOneTitle" />
+                  </h3>
+                  <InlineTextarea name="blockOneText" />
+                </div>
+                <div className={styles.col}>
+                  <h3>
+                    <InlineText name="blockTwoTitle" />
+                  </h3>
+                  <InlineTextarea name="blockTwoText" />
+                </div>
+                <div className={styles.col}>
+                  <h3>
+                    <InlineText name="blockThreeTitle" />
+                  </h3>
+                  <InlineTextarea name="blockThreeText" />
+                </div>
+              </div>
+            </div>
+          </section>
 
-						{/* TODO: Convert to BLOCKS */}
-						<div className={styles.container}>
-							<div className={styles.row}>
-								<div className={styles.col}>
-									<h3>
-										<InlineText name="blockFourTitle" />
-									</h3>
-									<InlineTextarea name="blockFourText" />
-								</div>
-								<div className={styles.col}>
-									<h3>
-										<InlineText name="blockFiveTitle" />
-									</h3>
-									<InlineTextarea name="blockFiveText" />
-								</div>
-								<div className={styles.col}>
-									<h3>
-										<InlineText name="blockSixTitle" />
-									</h3>
-									<InlineTextarea name="blockSixText" />
-								</div>
-							</div>
-						</div>
-					</section>
+          <div className={styles.image_break} />
 
-					<div className={styles.image_break} />
+          <section className={styles.about} id="about">
+            <div className={styles.about_intro}>
+              <h2>
+                <InlineText name="aboutTitle" />
+              </h2>
+              <InlineTextarea name="aboutText" />
+            </div>
 
-					<section className={styles.about}>
-						<div className={styles.about_intro}>
-							<h2>
-								<InlineText name="contactTitle" />
-							</h2>
-							<InlineTextarea name="contactText" />
-						</div>
+            {/* TODO: Convert to BLOCKS */}
+            <div className={styles.container}>
+              <div className={styles.row}>
+                <div className={styles.col}>
+                  <h3>
+                    <InlineText name="blockFourTitle" />
+                  </h3>
+                  <InlineTextarea name="blockFourText" />
+                </div>
+                <div className={styles.col}>
+                  <h3>
+                    <InlineText name="blockFiveTitle" />
+                  </h3>
+                  <InlineTextarea name="blockFiveText" />
+                </div>
+                <div className={styles.col}>
+                  <h3>
+                    <InlineText name="blockSixTitle" />
+                  </h3>
+                  <InlineTextarea name="blockSixText" />
+                </div>
+              </div>
+            </div>
+          </section>
 
-						<ContactForm />
-					</section>
+          <div className={styles.image_break} />
 
-					{/* TODO: Get current date for copyright */}
-					<footer className={styles.footer}>
-						<span className={styles.footer_text}>
-							&copy; 2020 <InlineText name="companyName" />
-						</span>
-					</footer>
+          <section className={styles.about} id="contact">
+            <div className={styles.about_intro}>
+              <h2>
+                <InlineText name="contactTitle" />
+              </h2>
+              <InlineTextarea name="contactText" />
+            </div>
 
+            <ContactForm />
+          </section>
 
-				</main>
-			</InlineForm>
-		</div>
-	);
+          {/* TODO: Get current date for copyright */}
+          <footer className={styles.footer}>
+            <span className={styles.footer_text}>
+              &copy; 2020 <InlineText name="companyName" />
+            </span>
+          </footer>
+        </main>
+      </InlineForm>
+    </div>
+  );
 }
 
 /**
  * Fetch data with getStaticProps based on 'preview' mode
  */
 export const getStaticProps: GetStaticProps = async function ({
-	preview,
-	previewData,
+  preview,
+  previewData,
 }) {
-	if (preview) {
-		return getGithubPreviewProps({
-			...previewData,
-			fileRelativePath: 'content/home.json',
-			parse: parseJson,
-		});
-	}
-	return {
-		props: {
-			sourceProvider: null,
-			error: null,
-			preview: false,
-			file: {
-				fileRelativePath: 'content/home.json',
-				data: (await import('../content/home.json')).default,
-			},
-		},
-	};
+  if (preview) {
+    return getGithubPreviewProps({
+      ...previewData,
+      fileRelativePath: "content/home.json",
+      parse: parseJson,
+    });
+  }
+  return {
+    props: {
+      sourceProvider: null,
+      error: null,
+      preview: false,
+      file: {
+        fileRelativePath: "content/home.json",
+        data: (await import("../content/home.json")).default,
+      },
+    },
+  };
 };
