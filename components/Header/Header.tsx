889930d16517bdb79c90e94b273bbc96c280c89d
Ginger Ben <ben.wilson@aerian.com>
2020-09-29 09:23:03 +0100

    types

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,13 @@ import { useRouter } from 'next/router';
 import { InlineText } from 'react-tinacms-inline';
 import styles from './Header.module.scss';
 
-export const Header =  ({ servicesTitle, aboutTitle, contactTitle }) => {
+interface HeaderProps {
+	servicesTitle: string;
+	aboutTitle: string;
+	contactTitle: string;
+}
+
+export const Header:React.FC<HeaderProps> =  ({ servicesTitle, aboutTitle, contactTitle }) => {
 	const router = useRouter();
 	const { languageCode } = router.query;
 
