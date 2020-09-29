dca4cbe27b2467f3f7219549c27058202639e257
Ginger Ben <ben.wilson@aerian.com>
2020-09-29 09:25:39 +0100

    more types

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,15 +1,16 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { useRouter } from 'next/router';
 import { InlineText } from 'react-tinacms-inline';
 import styles from './Header.module.scss';
 
 interface HeaderProps {
-	servicesTitle: string;
-	aboutTitle: string;
-	contactTitle: string;
+	servicesTitle?: string;
+	aboutTitle?: string;
+	contactTitle?: string;
 }
 
-export const Header:React.FC<HeaderProps> =  ({ servicesTitle, aboutTitle, contactTitle }) => {
+export const Header: React.FC<HeaderProps> = ({ servicesTitle, aboutTitle, contactTitle }) => {
 	const router = useRouter();
 	const { languageCode } = router.query;
 
