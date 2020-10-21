ac9aa887ba9083dc37c3780a343f87f90a5caec8
Ginger Ben <ben.wilson@aerian.com>
2020-10-21 21:52:53 +0100

    translate contact form

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -2,19 +2,21 @@ import React, { useState } from 'react';
 import styles from './ContactForm.module.scss';
 import LoadingIndicator from '../LoadingIndicator';
 import { InlineText } from 'react-tinacms-inline';
+import useFieldName from '../../utils/useFieldName';
 
 export const ContactForm = () => {
 	const [status, setStatus] = useState('initial');
+	const { getFieldName } = useFieldName();
 
 	const submitForm = (e) => {
 		e.preventDefault();
 
 		setStatus('loading');
-        
+
 		const form = e.target;
 		const data = new FormData(form);
 		const xhr = new XMLHttpRequest();
-        
+
 		xhr.open(form.method, form.action);
 		xhr.setRequestHeader('Accept', 'application/json');
 		xhr.onreadystatechange = () => {
@@ -26,7 +28,7 @@ export const ContactForm = () => {
 				setStatus('error');
 			}
 		};
-        
+
 		xhr.send(data);
 	};
 
@@ -37,16 +39,28 @@ export const ContactForm = () => {
 				action={process.env.CONTACT_FORM_URL}
 				method="POST"
 			>
-				<label className={styles.label}><InlineText name="Name label" /></label>
+				<label className={styles.label}>
+					<InlineText name={getFieldName('nameLabel')} />
+				</label>
 				<input type="name" name="name" className={styles.input} />
-				<label className={styles.label}><InlineText name="Email label" /></label>
+				<label className={styles.label}>
+					<InlineText name={getFieldName('emailLabel')} />
+				</label>
 				<input type="email" name="email" className={styles.input} />
-				<label className={styles.label}><InlineText name="Message label" /></label>
+				<label className={styles.label}>
+					<InlineText name={getFieldName('messageLabel')} />
+				</label>
 				<textarea name="message" rows={5} className={styles.input} />
 
-				{status === 'initial' && <button className={styles.button}><InlineText name="Submit label" /></button>}
+				{status === 'initial' && (
+					<button className={styles.button}>
+						<InlineText name={getFieldName('submitLabel')} />
+					</button>
+				)}
 				{status === 'loading' && <LoadingIndicator />}
-				{status === 'success' && <InlineText name="thanksMessage" />}
+				{status === 'success' && (
+					<InlineText name={getFieldName('thanksMessage')} />
+				)}
 				{status === 'error' && <p>Ooops! There was an error.</p>}
 			</form>
 		</div>
