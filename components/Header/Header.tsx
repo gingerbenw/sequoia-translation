37e20f2952ab491d37b81e675949353f524e9bd5
Ginger Ben <ben.wilson@aerian.com>
2020-10-21 21:56:59 +0100

    update translations

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -11,25 +11,32 @@ interface HeaderProps {
 
 const formatLink = (link: string) => '#' + link;
 
-export const Header: React.FC<HeaderProps> = ({ servicesTitle, aboutTitle, contactTitle }) => (
-	<header className={styles.header}>
-		<div className={styles.header_inner}>
-			<h1 className={styles.title}>
-				<InlineText name="title" />
-			</h1>
-			<nav className={styles.nav}>
-				<ul className={styles.menu}>
-					<a href={formatLink(servicesTitle)}>
-						<li>{servicesTitle}</li>
-					</a>
-					<a href={formatLink(aboutTitle)}>
-						<li>{aboutTitle}</li>
-					</a>
-					<a href={formatLink(contactTitle)}>
-						<li>{contactTitle}</li>
-					</a>
-				</ul>
-			</nav>
-		</div>
-	</header>
-);
+export const Header: React.FC<HeaderProps> = ({
+	servicesTitle,
+	aboutTitle,
+	contactTitle,
+}) => {
+
+	return (
+		<header className={styles.header}>
+			<div className={styles.header_inner}>
+				<h1 className={styles.title}>
+					<InlineText name={'title'} />
+				</h1>
+				<nav className={styles.nav}>
+					<ul className={styles.menu}>
+						<a href={formatLink(servicesTitle)}>
+							<li>{servicesTitle}</li>
+						</a>
+						<a href={formatLink(aboutTitle)}>
+							<li>{aboutTitle}</li>
+						</a>
+						<a href={formatLink(contactTitle)}>
+							<li>{contactTitle}</li>
+						</a>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	);
+};
